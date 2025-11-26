0eed1cca3f77433d921dd05989c485b9ecd156e0
Pranay Gundu <pranaygundu423@gmail.com>
2025-11-27 01:29:15 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='build ml project to predict swiggy delivery time',
+    author='Pranay Gundu',
+    license='',
+)
